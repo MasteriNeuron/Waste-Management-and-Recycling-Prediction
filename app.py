bb19df5c5cd773ed2914bf6aa85b449d9c86f652
Master_Neuron <tcsshubham5@gmail.com>
2025-09-09 13:14:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -341,4 +341,5 @@ def get_logs():
 # ----------------- Entrypoint -----------------
 if __name__ == '__main__':
     load_artifacts_global()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
